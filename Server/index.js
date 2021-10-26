9b38769c83d7608e98754be8534ea19473712450
Shalev Hadar <shdar94@gmail.com>
2021-10-26 14:42:57 +0300

    Now with dotenv

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
 import postAuths from './routes/Auths.js'
+import dotenv from "dotenv";
+
 
 const app = express();
 // Parse URL-encoded bodies (as sent by HTML forms)
@@ -14,9 +16,12 @@ app.use(express.urlencoded({extended: true, limit: '100mb'}));
 app.use(express.json());
 app.use(cors());
 app.options('*', cors())
+dotenv.config();
 
-const CONNECTION_URL = 'mongodb+srv://bete:bete123456@cluster0.jxiat.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const CONNECTION_URL = 'mongodb+srv://bete:'+process.env.DB_PASS+'@cluster0.jxiat.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
+// const pass = process.env.DB_PASS;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:`+PORT)))
